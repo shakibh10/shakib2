516be0b57f8f74b5f654a9aabd9ac7d06eb761ec
shakibh10 <billalsikdar03@gmail.com>
2023-08-09 10:14:39 +0600

    first commit

---
+++ string1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{
+    int x=2,y=3,*p,*q;
+
+    p=&x;
+    q=&y;
+    p=q;
+
+    printf("%d %d %d %d",x,y,*p,*q);
+
+    *p=3;
+    *q=4;
+    x=y;
+    printf("\n%d %d %d %d",*p,*q,x,y);
+
+    getch();
+}
